11d052e64510a21517e4a2028ddb51bb3c2d5ba8
Aleksandr <sancepance.2001@mail.ru>
2021-10-19 16:17:04 +0300

    Created layout and classes

---
+++ app/src/main/java/com/example/test3/ScoreLogic.java
@@ -0,0 +1,13 @@
+package com.example.test3;
+
+public class ScoreLogic {
+    private int score = 0;
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
