886fef56d9b293e6c6da34033f53b2cc81bf25d0
Aleksandr <sancepance.2001@mail.ru>
2021-10-19 16:29:11 +0300

    Created method in MainLogic

--- app/src/main/java/com/example/test3/MainLogic.java
+++ app/src/main/java/com/example/test3/MainLogic.java
@@ -16,4 +16,20 @@ public class MainLogic {
             0, 0, 0, 0, 0, 0,
             0
     };
+
+    // Проверка наличия введеного слова
+    public int wordSearch(String word) {
+        for(int i = 0; i < words.length; i++){
+            if(words[i].equals(word)){
+                if (marks[i] == 0) {
+                    marks[i] = 1;
+                    return i;
+                }else{
+                    return -2;
+                }
+            }
+        }
+        return -1;
+    }
+
 }
